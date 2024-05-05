05a94e60c982974bfbbe18c5a11e0a5115162e93
user530 <m.timarsuev@gmail.com>
2024-05-05 23:24:05 +0300

    Adjusted graphsql dependencies + fixed graphql module + created basic schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { MonstersModule } from './monsters/monsters.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql')
     }),
     MonstersModule
